fca58564dd7efed3c80c48e6058c3b5e1384d9c1
Naveen <hi.to.cloud@gmail.com>
2024-10-27 09:16:26 +0530

    Added Expense-TF-FE-ASG

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  type    = string
+  default = "expense"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "common_tags" {
+  type = map(any)
+  default = {
+    Project     = "expense",
+    Environment = "dev",
+    Terraform   = "true",
+    Component   = "frontend",
+    SERVER      = "fe"
+  }
+}
+
+variable "zone_name" {
+  default = "step-into-iot.cloud"
+}
